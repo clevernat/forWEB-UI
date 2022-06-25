71c591c4cc8a4fbf9efacdd47f13932b34c504d0
clevernat <clevernat1@gmail.com>
2022-06-25 11:45:39 +0000

    Initial Commit

---
+++ dist/js/app.js
@@ -0,0 +1,10 @@
+const labelDate = document.querySelector('.date');
+
+const currentDate = new Date()
+
+const options = {
+    month: 'short',
+    year: '2-digit',
+}
+
+labelDate.textContent = new Intl.DateTimeFormat(navigator.languages, options).format(currentDate)
